7b56ccc10d26ee2342324fb2ba54e7606904f87f
Amrit Singh Panesar <66624024+ASP1527@users.noreply.github.com>
2020-07-29 13:42:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-'''
+
 # List of modules that I have imported
 import os
-import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
 from bs4 import BeautifulSoup
 import requests
+from time import time, ctime
 
 
 # Function that allows the assistant to speak
@@ -37,6 +37,42 @@ def remove_file():
     os.remove('voice.mp3')
 
 
+'''
+Functions of what it can do
+'''
+
+
+def get_weather_today():
+    page = requests.get('https://www.bbc.co.uk/weather/2643743')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup)
+    info = soup.find_all(class_='wr-value--temperature--c')
+    # print(info)
+    high_today = info[0].get_text()
+    low_today = info[1].get_text()
+    print(high_today)
+    print(low_today)
+    tie = time()
+    timeData = ctime(tie)
+    splitData = timeData.split(" ")
+    times = splitData[3]
+    times = times.split(":")
+    hour = times[0]
+    hour = int(hour)
+    if hour > 17 or hour < 5:
+        speak("Right now, it is " + high_today)
+        remove_file()
+    else:
+        speak("Today, right now, it is " +
+              high_today + ". Today there is a low of " + low_today)
+        remove_file()
+
+
+'''
+end of functions
+'''
+
+
 # Running an infinite loop in which the assistant is called when the keyword is said
 global run
 run = False
@@ -61,8 +97,9 @@ def main_loop():
         text = get_audio()
 
         if "weather" in text:
-            speak("28 degrees")
-            print("28 degrees")
+            get_weather_today()
+        else:
+            speak("I am not sure how to do that at the moment")
             remove_file()
 
         running_main_loop = False
@@ -71,4 +108,3 @@ def main_loop():
 
 
 calling_assistant()
-'''
